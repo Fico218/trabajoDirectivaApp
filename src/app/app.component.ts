e2f8d664f486ba57a74a9f072c161db1c8339d4f
Jorge Sandoval <jrss317031@gmail.com>
2023-09-08 12:02:14 -0500

    src/app/app.component.html src/app/app.component.ts src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo="Registro de Usuarios";
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas:any;
+ 
+  constructor(){
+
+    this.entradas=[
+      {titulo:"Python cada día mas presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez mas funcional"},
+      {titulo:"Kotlyn potencia para tus apps"},
+      {titulo:"¿Dónde quedó Pascall?"},
+    ]
+  }
+
+  registrarUsuario(){
+
+    this.registrado=true
+
+    this.mensaje="usuario registrado con éxito"
+  }
 }
